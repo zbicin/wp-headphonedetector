46f9063e3c80a04452af980915c03f4be873404b
Krzysiek <fistasheq@gmail.com>
2014-02-17 16:13:20 +0100

    app template; basic detection

---
+++ HeadphonesDetector/AudioPathResolver.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Windows.Phone.Media.Devices;
+
+namespace HeadphonesDetector
+{
+    public class AudioPathResolver : INotifyPropertyChanged
+    {
+        private string _audioPath;
+        public string AudioPath
+        {
+            get { return _audioPath; }
+            set
+            {
+                _audioPath = value;
+                NotifyOfPropertyChange("AudioPath");
+            }
+        }
+
+        public AudioPathResolver()
+        {
+            AudioPath = AudioEndPointToString(AudioRoutingManager.GetDefault().GetAudioEndpoint());
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        protected virtual void NotifyOfPropertyChange(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                Deployment.Current.Dispatcher.BeginInvoke(() =>
+                {
+                    handler(this, new PropertyChangedEventArgs(propertyName));
+                });
+                
+            }
+        }
+
+        public void AddNotificationHandler()
+        {
+            AudioRoutingManager.GetDefault().AudioEndpointChanged += AudioEndpointChanged_Handler;
+        }
+
+        public void RemoveNotificationHandler()
+        {
+            AudioRoutingManager.GetDefault().AudioEndpointChanged -= AudioEndpointChanged_Handler;
+        }
+
+        public void AudioEndpointChanged_Handler(AudioRoutingManager sender, object args)
+        {
+            var AudioEndPoint = sender.GetAudioEndpoint();
+            AudioPath = AudioEndPointToString(AudioEndPoint);
+        }
+
+        private string AudioEndPointToString(AudioRoutingEndpoint AudioEndPoint)
+        {
+            string audioEndPointString;
+            switch (AudioEndPoint)
+            {
+                case AudioRoutingEndpoint.Default:
+                    {
+                        audioEndPointString = "Default";
+                        break;
+                    }
+                case AudioRoutingEndpoint.Earpiece:
+                    {
+                        audioEndPointString = "Earpiece";
+                        break;
+                    }
+                case AudioRoutingEndpoint.Speakerphone:
+                    {
+                        audioEndPointString = "Speakerphone";
+                        break;
+                    }
+                case AudioRoutingEndpoint.Bluetooth:
+                    {
+                        audioEndPointString = "Bluetooth";
+                        break;
+                    }
+                case AudioRoutingEndpoint.WiredHeadset:
+                    {
+                        audioEndPointString = "WiredHeadset";
+                        break;
+                    }
+                case AudioRoutingEndpoint.WiredHeadsetSpeakerOnly:
+                    {
+                        audioEndPointString = "WiredHeadsetSpeakerOnly";
+                        break;
+                    }
+                case AudioRoutingEndpoint.BluetoothWithNoiseAndEchoCancellation:
+                    {
+                        audioEndPointString = "BluetoothWithNoiseAndEchoCancellation";
+                        break;
+                    }
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+            return audioEndPointString;
+        }
+    }
+}
---
+++ HeadphonesDetector/MainPage.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using HeadphonesDetector.Resources;
+using Windows.Phone.Media.Devices;
+
+namespace HeadphonesDetector
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        public String HeadphonesState;
+
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+
+           
+        }
+
+        private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Load");
+            //(this.LayoutRoot.DataContext as AudioPathResolver).AddNotificationHandler();
+        }
+
+        private void PhoneApplicationPage_Unloaded(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Unload");
+            //(this.LayoutRoot.DataContext as AudioPathResolver).RemoveNotificationHandler();
+        
+
+        }
+
+        private void btnTest_Click(object sender, RoutedEventArgs e)
+        {
+            AudioRoutingEndpoint audioRoutingEndpoint = AudioRoutingManager.GetDefault().GetAudioEndpoint();
+            
+            if (audioRoutingEndpoint == AudioRoutingEndpoint.Default
+                || audioRoutingEndpoint == AudioRoutingEndpoint.Speakerphone)
+            {
+                MessageBox.Show("Speakers on!");
+            }
+            else
+            {
+                MessageBox.Show("Headphones plugged in");
+            }
+        }
+
+        // Sample code for building a localized ApplicationBar
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // Create a new button and set the text value to the localized string from AppResources.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Create a new menu item with the localized string from AppResources.
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+    }
+}
